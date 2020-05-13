131ab833e24114d237d9e5b6de99eb7c30afefdb
Laura Apetroaei <laura.apetroaei@sfappworks.com>
2020-05-13 13:59:08 +0300

    Try https reedirect

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,7 +8,12 @@ router.use(express.static(path.join(__dirname, '../../../public'), { maxAge: 30 
 router.use(express.static(path.join(__dirname, '../../../dist'), { maxAge: 30 * 60 * 60 * 24 * 1000 }));
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../dist/index.html'));
+    // Force redirect to HTTPS
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+        res.sendFile(path.join(__dirname, '../../../dist/index.html'));
+    }
 });
 
 module.exports = router;
